36f05c706dca150e7e7cc82dc1f2fa7bf30a204d
Ruslan Nabiullin <whitebarsfan@gmail.com>
2022-08-05 18:20:54 +0300

    Initial Commit

---
+++ ChangeViewColor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChangeViewColor
+//
+//  Created by Mac on 05/08/2022.
+//  Copyright © 2022 Russel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
